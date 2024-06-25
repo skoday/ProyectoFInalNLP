2d22cf76cfdfb73d348941b12c8eea57473f40c6
Nestor Martínez <76193358+skoday@users.noreply.github.com>
2024-06-25 10:47:35 -0600

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import requests
 
-# Título de la aplicación
-st.title("Chat con API de FastAPI")
+st.title("Chat Conceptos Matematicos")
 
-# Descripción
-st.write("Esta aplicación permite hacer preguntas a una API de FastAPI y obtener respuestas.")
+st.write("Pregunta lo conceptos sobre aritmetica, algebra lineal, probabilidad, calculo y geometria analitica")
 
 # Función para obtener la respuesta de la API
 def obtener_respuesta(pregunta):
@@ -17,21 +15,18 @@ def obtener_respuesta(pregunta):
     else:
         return {"error": "Error al comunicarse con la API."}
 
-# Interfaz de usuario tipo chat
 st.subheader("Chat")
 
-# Lista para almacenar el historial de mensajes
 historial = []
 
-# Función para mostrar los mensajes en el formato de chat
 def mostrar_mensaje(texto, emisor, identificador):
     if emisor == "Usuario":
-        st.text_area(f"Tú escribes {identificador}:", value=texto, height=100, key=f"user_{identificador}")
+        st.text_area(f"Tu mensaje {identificador}:", value=texto, height=100, key=f"user_{identificador}")
     elif emisor == "Bot":
-        st.text_area(f"Bot responde {identificador}:", value=texto, height=100, key=f"bot_{identificador}")
+        st.text_area(f"Respuesta {identificador}:", value=texto, height=100, key=f"bot_{identificador}")
 
 # Input de usuario
-pregunta = st.text_input("Tú escribes:", "")
+pregunta = st.text_input("Tu mensaje:", "")
 
 # Botón para enviar la pregunta
 if st.button("Enviar"):
@@ -39,20 +34,16 @@ if st.button("Enviar"):
         # Agregar pregunta al historial y mostrarla
         historial.append(("Usuario", pregunta))
 
-        # Obtener respuesta del bot
         respuesta = obtener_respuesta(pregunta)
         if "error" in respuesta:
-            # Mostrar error si no se pudo obtener la respuesta
             historial.append(("Bot", f"**Error:** {respuesta['error']}"))
         else:
-            # Mostrar la respuesta del bot
             respuesta_texto = respuesta.get("answer", "No se obtuvo respuesta.")
-            historial.append(("Bot", f"**Respuesta:** {respuesta_texto}"))
+            historial.append(("Bot", f"Respuesta: {respuesta_texto}"))
 
     else:
         st.text_area("Bot responde:", "Por favor, ingresa una pregunta.")
 
-# Mostrar historial de mensajes
 if historial:
     st.subheader("Historial del Chat")
     for i, mensaje in enumerate(historial):
